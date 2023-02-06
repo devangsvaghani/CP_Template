de81c5da19e6178a48a7a20578d0543186664f92
devangsvaghani <112165535+devangsvaghani@users.noreply.github.com>
2023-02-06 17:59:56 +0530

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define pb push_back
+#define em emplace_back
+#define fi(l, r) for(ll i=l; i<r; i++)
+#define fir(r, l) for(ll i=r; i>=l; i--)
+template<typename T> void input(vector<T>& t) { for (auto& v : t) cin >> v; }
+
+void solve(){
+
+}
+
+int main(){
+    ll t;
+    cin >> t;
+    // t = 1;
+    while(t--){
+        solve();
+    }
+    return 0;
+}
