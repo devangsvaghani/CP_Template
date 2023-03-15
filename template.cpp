e2107cb310133ac719a830963f04763d96270471
devangsvaghani <112165535+devangsvaghani@users.noreply.github.com>
2023-03-15 19:30:25 +0530

    Update template.cpp

--- template.cpp
+++ template.cpp
@@ -8,6 +8,10 @@ using namespace std;
 #define eb emplace_back
 #define yes cout << "YES" << endl;
 #define no cout << "NO" << endl;
+#define fi(l, r) for(ll i=l;i<r;i++)
+#define fj(l, r) for(ll j=l;j<r;j++)
+#define fir(r, l) for(ll i=r;i>=l;i--)
+#define fjr(r, l) for(ll j=r;j>=l;j--)
 
 void solve(){
     
@@ -22,4 +26,3 @@ int main(){
     }
     return 0;
 }
-
