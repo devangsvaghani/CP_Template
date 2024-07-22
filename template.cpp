ab6da9a6e56ae2135e4cb854b57e2e85f03028f7
Devang Vaghani <devangsvaghani@gmail.com>
2024-07-22 23:52:02 +0530

    VS Code Snippets Started Added

--- template.cpp
+++ template.cpp
@@ -1,12 +1,5 @@
 #include <bits/stdc++.h>
-
-// #include<ext/pb_ds/assoc_container.hpp>
-// #include<ext/pb_ds/tree_policy.hpp>
-
 using namespace std;
-// using namespace __gnu_pbds;
-
-// typedef tree<int, null_type, less<int>, rb_tree_tag, tree_order_statistics_node_update> pbds; // find_by_order, order_of_key
 
 #ifndef ONLINE_JUDGE
 #include "dev/debug.hpp"
@@ -14,7 +7,7 @@ using namespace std;
 #define debug(...) 8
 #endif
 
-#define devangsvaghani ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define DSV ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
 #define pb push_back
 #define all(x) (x).begin(), (x).end()
 
@@ -24,22 +17,14 @@ typedef long double lld;
 
 #define yes cout << "YES\n";
 #define no cout << "NO\n";
+#define first F
+#define second S
 #define f(i, x, n)       for (ll i = x; i < n; i++)
 #define rf(i, x, n)      for (ll i = x; i >= n; i--)
 
 const ll mod = 1e9+7;
 const ll mod2 = 998244353;
-const ll inf = 1e18;
-const ll N = 1e5+5;
-
-
-
-bool sortbysec(const pair<int,int> &a,const pair<int,int> &b) { return (a.second < b.second); }
-ll power(ll n,ll a){ ll res=1; while(a){ if(a%2) res*=n,a--; else   n*=n,a/=2;} return res;}
-ll modpower(ll n,ll a,ll p){ ll res=1; while(a){ if(a%2) res= ((res*n)%p) ,a--; else n=((n*n)%p),a/=2;} return res;}
-ll fastprime(ll a){ if (((a != 2) && (a % 2 == 0)) || (a < 2)) return 0;  for(ll i = 3; i * i <= a; i += 2) if(a % i == 0) return 0; return 1;}
-ll mod_mul_inverse(ll a,ll b) {return modpower(a , b-2, b);}
-ll gcd(ll a, ll b){return (b == 0 ? a : gcd(b, a % b));}
+const ll N = 1e6+5;
 
 void init(){
 
@@ -50,14 +35,20 @@ void solve(){
 }
 
 int main() {
-  devangsvaghani;
+  DSV;
+
+  // #ifndef ONLINE_JUDGE
+  //   freopen("input.txt", "r", stdin);
+  //   freopen("output.txt", "w", stdout);
+  //   freopen("Error.txt", "w", stderr);
+  // #endif
 
   // init();
   
   ll t = 1;
   cin >> t;
   for (ll T = 1; T <= t; T++) {
-    // cout << "Case #" << T << ": ";
+    // debug(T);
     solve();
   }
   return 0;
