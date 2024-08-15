7391e6a0d7a90ab52b31f289022f0314f3bae99f
Devang Vaghani <devangsvaghani@gmail.com>
2024-08-15 21:46:46 +0530

    vs code snippet file added

--- Range Query/fenwick_tree.cpp
+++ Range Query/fenwick_tree.cpp
@@ -30,3 +30,19 @@ class fenwick {
 		v += val;
 	}
 };
+
+
+
+//short template
+struct FT {
+    vector<ll> s;
+    FT(int n) : s(n) {}
+    void update(int pos, ll dif) { // a[pos] += dif
+        for (; pos < size(s); pos |= pos + 1) s[pos] += dif;
+    }
+    ll query(int pos) { // sum of values in [0, pos)
+        ll res = 0;
+        for (; pos > 0; pos &= pos - 1) res += s[pos-1];
+        return res;
+    }
+};
--- template.cpp
+++ template.cpp
@@ -1,47 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Ordered Set
-// #include<ext/pb_ds/assoc_container.hpp>
-// #include<ext/pb_ds/tree_policy.hpp>
-// using namespace __gnu_pbds; // find_by_order && order_of_key
-// template<class T> using ordered_set = tree<T, null_type, less<T>, rb_tree_tag,tree_order_statistics_node_update>;
-
 #ifndef ONLINE_JUDGE
 #include "dev/debug.hpp"
 #else
 #define debug(...) 8
 #endif
 
-#define DSV ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
-#define pb push_back
-#define all(x) (x).begin(), (x).end()
-
 typedef long long ll;
 typedef unsigned long long ull;
 typedef long double lld;
 
-#define yes cout << "YES\n";
-#define no cout << "NO\n";
+#define all(x) (x).begin(), (x).end()
 #define F first
 #define S second
-#define f(i, x, n)       for (ll i = x; i < n; i++)
-#define rf(i, x, n)      for (ll i = x; i >= n; i--)
+#define f(i, x, n) for (ll i = x; i < n; i++)
+#define rf(i, x, n) for (ll i = x; i >= n; i--)
 
 const ll mod = 1e9+7;
 const ll mod2 = 998244353;
 const ll N = 1e6+5;
 
 void init(){
-
+    
 }
 
 void solve(){
-  
+    
 }
 
 int main() {
-  DSV;
+  ios_base::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL);
 
   // #ifndef ONLINE_JUDGE
   //   freopen("input.txt", "r", stdin);
@@ -49,7 +38,7 @@ int main() {
   //   freopen("Error.txt", "w", stderr);
   // #endif
 
-  // init();
+  init();
   
   ll t = 1;
   cin >> t;
