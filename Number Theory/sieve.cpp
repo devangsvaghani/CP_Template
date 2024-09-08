1d1ac5192ded2ccf1da56806ecb2ace4fe9d1dd0
Devang Vaghani <devangsvaghani@gmail.com>
2024-09-08 15:50:07 +0530

    Optimised sieve added

--- Number Theory/sieve.cpp
+++ Number Theory/sieve.cpp
@@ -12,3 +12,28 @@ void f_count_divisors(){
 bool isPrime(ll x){
     return (count_divisors[x] == 2);
 }
+
+
+
+// Optimised Sieve specially for N = 1e8
+const ll N = 1e8+1;
+bool prime[N];
+vector<int> ps;
+
+void sieve(){
+    for(ll i = 0; i < N; i++) prime[i] = true;
+    for(ll i = 3; i * i < N; i += 2){
+        if(prime[i / 2]){
+            for(ll j = i * i; j < N; j += i * 2){
+                prime[j / 2] = false;
+            }
+        }
+    }
+
+    ps.push_back(2);
+    for(ll i = 3; i < N; i += 2){
+        if(prime[i / 2]){
+            ps.push_back(i);
+        }
+    }
+}
