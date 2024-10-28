f5e18e2f8c2a8571dbfac49746db177f73c86dfd
Devang Vaghani <devangsvaghani@gmail.com>
2024-10-28 16:09:08 +0530

    combinatorics added

---
+++ Number Theory/combinatorics.cpp
@@ -0,0 +1,21 @@
+// Factorial & inverse Factorial Calculation
+const long long factSize = 2e6 + 5;
+long long fact[factSize];
+long long invFact[factSize];
+
+void preComputeFact(){
+    fact[0] = 1;
+    for(long long i = 1; i < factSize; ++i){
+        fact[i] = (fact[i - 1] * i) % mod;
+    }
+    invFact[factSize - 1] = binPowerMod(fact[factSize - 1], mod - 2, mod);
+    for(long long i = factSize - 2; i >= 0; --i){
+        invFact[i] = (invFact[i + 1] * (i + 1)) % mod;
+    }
+}
+
+// nCr Calculation
+long long nCr(long long n, long long r){
+    if(r < 0 || r > n) return 0ll;
+    return ((fact[n] * invFact[n - r]) % mod * invFact[r]) % mod; 
+}
--- Number Theory/power.cpp
+++ Number Theory/power.cpp
@@ -1,20 +1,21 @@
-ll power(ll a, ll b){
-    ll res = 1;
+// Binary Exponentiation with modulo
+long long binPowerMod(long long a, long long b, long long m){
+    long long res = 1;
+    a %= m;
     while(b > 0){
-        if(b & 1) res = (res * a);
-        a = (a * a);
+        if(b & 1) res = (res * a) % m;
+        a = (a * a) % m;
         b >>= 1;
     }
     return res;
 }
 
-
-
-ll modPower(ll a, ll b, ll m){
-    ll res = 1;
+// Binary Exponentiation without modulo
+long long binPower(long long a, long long b){
+    long long res = 1;
     while(b > 0){
-        if(b & 1) res = (res * a) % m;
-        a = (a * a) % m;
+        if(b & 1) res = (res * a);
+        a = (a * a);
         b >>= 1;
     }
     return res;
